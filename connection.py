5f8685db803162b3d2f465d3766a6f18f336fc02
medboss2008 <kanikiken550720@gmail.com>
2024-08-29 22:40:47 +0100

    Update database connection settings

--- connection.py
+++ connection.py
@@ -1,11 +1,13 @@
+import os
 import mysql.connector
 
 # Connexion à la base de données
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="Bossmed@2008",
-    database="mb_design"
+    host=os.getenv('DB_HOST', 'localhost'),
+    port=os.getenv('DB_PORT', '3306'),
+    user=os.getenv('DB_USER', 'root'),
+    password=os.getenv('DB_PASSWORD', 'Bossmed@2008'),
+    database=os.getenv('DB_NAME', 'mb_design')
 )
 
 # Créer un curseur pour exécuter les commandes SQL
